f2d236c97156f444e2365addbee10562ebb94592
jokereven <zjing125.cn@gmail.com>
2023-08-18 23:41:56 +0800

    feat: add quill

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+
 import Index from "@/components/layouts/AppLayout";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+function Q() {
+  const [value, setValue] = useState("");
+
+  // https://juejin.cn/post/7195124289501134905
+  const modules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike", "blockquote", ""],
+      [{ header: [1, 2, false] }],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+      [{ size: ["small", false, "large", "huge"] }],
+      [{ color: [] }, { background: [] }],
+    ],
+  };
+
+  return <ReactQuill theme="snow" modules={modules} value={value}></ReactQuill>;
+}
 
 export default function Page() {
   return (
     <Index>
-      <div>post</div>
+      <Q />
     </Index>
   );
 }
--- components/layouts/AppLayout/index.tsx
+++ components/layouts/AppLayout/index.tsx
@@ -6,7 +6,7 @@ export default function Index({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       <Sidebar className="h-full w-auto" />
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full mr-[4.75rem]">
         <Header />
         {children}
       </div>
