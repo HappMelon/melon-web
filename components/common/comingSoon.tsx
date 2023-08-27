08ca33deabd878899e7078194ae13fb518fc6aba
jokereven <zjing125.cn@gmail.com>
2023-08-27 10:58:20 +0800

    feat: Issue #17

--- components/common/comingSoon.tsx
+++ components/common/comingSoon.tsx
@@ -1,23 +1,11 @@
 import Index from "@/components/layouts/AppLayout";
 
-export default function ComingSoon({ modelname }: { modelname: string }) {
+export default function ComingSoon() {
   return (
     <Index>
-      {/* TODO */}
-      {/* Comeing soon */}
       <div className="flex flex-col items-center">
-        <div className="text-[16px] font-medium mt-[27px] mb-[13px] text-center">
-          Coming soon
-        </div>
-        <img
-          className="w-[145px] h-[105px]"
-          src="https://ipfs.xlog.app/ipfs/bafkreiehjdts654n2gnovkwrcm6yhwbqntacezr4xu6p6nqvhncusz65ci"
-          alt=""
-        />
-        <div className="m-0 text-[14px] font-normal text-[#49454F] text-center max-w-[252px]">
-          Illustration The {modelname} will be released soon, thank you for your
-          patience.
-        </div>
+        <img className="w-[146px] h-[145px]" src="/comingSoon.png" alt="" />
+        <div className="font-normal text-[2.5rem]">Coming Soon!</div>
       </div>
     </Index>
   );
--- components/layouts/AppLayout/Nav.tsx
+++ components/layouts/AppLayout/Nav.tsx
@@ -129,30 +129,6 @@ export function Sidebar({ className, username }: SidebarProps) {
               </svg>
               Post
             </Button>
-            <Button
-              variant={
-                pathname === `/${username}`
-                  ? "secondary"
-                  : ("ghost" as unknown as any)
-              }
-              className="w-full justify-start"
-              onClick={() => SwitchRouter(`/${username}`)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-              </svg>
-              Account
-            </Button>
           </div>
         </div>
       </div>
