6ab69260c502d30851f94a45edefce5f5c0f516e
jokereven <zjing125.cn@gmail.com>
2023-08-28 16:59:12 +0800

    feat: move layout to layout

--- components/common/comingSoon.tsx
+++ components/common/comingSoon.tsx
@@ -1,12 +1,8 @@
-import Index from "@/components/layouts/AppLayout";
-
 export default function ComingSoon() {
   return (
-    <Index>
-      <div className="flex flex-col items-center">
-        <img className="w-[146px] h-[145px]" src="/comingSoon.png" alt="" />
-        <div className="font-normal text-[2.5rem]">Coming Soon!</div>
-      </div>
-    </Index>
+    <div className="flex flex-col items-center">
+      <img className="w-[146px] h-[145px]" src="/comingSoon.png" alt="" />
+      <div className="font-normal text-[2.5rem]">Coming Soon!</div>
+    </div>
   );
 }
