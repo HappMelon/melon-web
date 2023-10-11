2e80e4ce149473c7583f9358eb86bbd91a8b3611
4pmtong <web_chentong@163.com>
2023-10-11 22:59:31 +0800

    :art: update pnpm

--- web3/connect.ts
+++ web3/connect.ts
@@ -3,8 +3,10 @@ import { ethers } from "ethers";
 let provider: any;
 
 export const getProvider = async () => {
+  // @ts-ignore
   if (!window.ethereum) return;
 
+  // @ts-ignore
   const providerWeb3 = new ethers.providers.Web3Provider(window.ethereum);
   provider = providerWeb3;
 
