0e2dae2debf74f38b0ab2e260b581f51db496ce1
yeweicheng <ywc959@gmail.com>
2023-11-22 18:08:19 +0800

    chore: update popular style

--- components/thread/comment/TopicsAndAuthors.tsx
+++ components/thread/comment/TopicsAndAuthors.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export async function TopicsAndAuthors({ className }: SidebarProps) {
-  let cl = `ml-[0.5rem] 2xl:ml-[1.875rem] pr-2`;
+  let cl = `ml-[0.5rem] 2xl:ml-[1.875rem] pr-2 max-w-[20rem]`;
   return (
     <div className={cn(className, cl)}>
       <HotTopics></HotTopics>
